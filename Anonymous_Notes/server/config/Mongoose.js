b16bd1aea7a6c0693c5d90196311546368be49f3
Hagan <haganpratt@gmail.com>
2018-03-02 12:28:03 -0500

    fm

--- Anonymous_Notes/server/config/Mongoose.js
+++ Anonymous_Notes/server/config/Mongoose.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const fs = require('fs');
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
-mongoose.connect('mongodb://localhost/notes', { useMongoClient: true }, () => console.log('Connected to notes. Well done.'));
+mongoose.connect("mongodb://localhost/iceCream", { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
-const models_path = __dirname + '/../models';
-
+var models_path = __dirname + "/../models";
 fs.readdirSync(models_path).forEach((file) => {
-    if (file.includes('.js')) {
-        console.log(`Loading ${file}...`);
-        require(`${models_path}/${file}`);
+    if (file.includes(".js")) {
+        console.log("loading " + file + "...");
+        require(models_path + "/" + file);
     }
 })
